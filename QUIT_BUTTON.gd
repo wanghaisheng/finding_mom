2474c57874d0ebebf04a59d43a4ba803b87faad5
unknown <red42king@gmail.com>
2024-05-22 00:12:18 -0600

    added some print statements for buttons

---
+++ QUIT_BUTTON.gd
@@ -0,0 +1,17 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.pressed.connect(self._button_pressed)
+
+	
+func _button_pressed():
+	print("Hello world!")
+	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+
