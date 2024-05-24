a484502027850708480dcfbd648f1d265f59cce1
unknown <red42king@gmail.com>
2024-05-24 09:42:46 -0600

    Demo complete

--- QUIT_BUTTON.gd
+++ QUIT_BUTTON.gd
@@ -3,15 +3,17 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	self.pressed.connect(self._button_pressed)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	self.pressed.connect(self._button_pressed)
+func _process(_delta):
+	pass
 
 	
 func _button_pressed():
-	print("Hello world!")
+	print("Quit")
+	# TODO: catch the notification with a confirmation box
 	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
-
+	get_tree().quit(0)
--- SETTINGS_BUTTON.gd
+++ SETTINGS_BUTTON.gd
@@ -3,12 +3,12 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	self.pressed.connect(self._button_pressed)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	self.pressed.connect(self._button_pressed)
+func _process(_delta):
+	pass
 
 	
 func _button_pressed():
