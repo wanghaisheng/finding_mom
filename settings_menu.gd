0132a4ffcf622a5476be1f13b41828d5b2905c73
unknown <red42king@gmail.com>
2024-12-06 14:34:50 -0700

    added new music and fixed up settings and gameplay

--- settings_menu.gd
+++ settings_menu.gd
@@ -2,8 +2,9 @@ extends CanvasLayer
 
 signal back_button_pressed
 
-var selectedResolution = ""
-var selectedViewportMode = ""
+var selectedResolution := "3840 X 2160"
+var selectedViewportMode := "Fullscreen"
+var selectedVolume := 0.0
 
 var config = ConfigFile.new()
 
@@ -18,8 +19,11 @@ func apply_settings():
 	_on_apply_button_pressed()
 
 func _on_apply_button_pressed():
+	# set resolution:
 	var sizes = selectedResolution.split(" X ", false, 2)
 	get_window().size = Vector2i(int(sizes[0]), int(sizes[1]))
+	
+	# set windowMode
 	var windowMode = Window.MODE_EXCLUSIVE_FULLSCREEN
 	match selectedViewportMode:
 		"Maximized":
@@ -33,23 +37,32 @@ func _on_apply_button_pressed():
 		"Exclusive Fullscreen":
 			windowMode = Window.MODE_EXCLUSIVE_FULLSCREEN
 	get_tree().root.mode = windowMode
-	save_config(selectedResolution, selectedViewportMode)
 	
-func save_config(resolution, viewport_mode):
+	# TODO: set master volume:
+	var master_index := AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_volume_db(master_index, selectedVolume)
+	
+	save_config(selectedResolution, selectedViewportMode, selectedVolume)
+	
+func save_config(resolution, viewport_mode, master_volume):
 	config.set_value("Settings", "resolution", resolution)
 	config.set_value("Settings", "viewport_mode", viewport_mode)
+	config.set_value("Settings", "master_volume", master_volume)
 	config.save("./settings.cfg")
 	
 func load_settings():
 	var err = config.load("./settings.cfg")
 	if err != OK:
+		print("failed to load settings from settings.cfg.\nDoes it exist?")
 		return
 
+	# retrieve the settings from the config file
 	for setting in config.get_sections():
 		selectedResolution = config.get_value("Settings", "resolution")
 		selectedViewportMode = config.get_value("Settings", "viewport_mode")
+		selectedVolume = config.get_value("Settings", "master_volume")
 
-	# next find the index of what they are and select them with this:
+	# next find the index of what they are and set them with this:
 	$ResolutionOptions.select(0)
 	$ViewportOptions.select(0)
 	for i in $ResolutionOptions.item_count:
@@ -58,6 +71,7 @@ func load_settings():
 	for i in $ViewportOptions.item_count:
 		if $ViewportOptions.get_item_text(i) == selectedViewportMode:
 			$ViewportOptions.select(i)
+	$VolumeSlider.value = selectedVolume
 
 func _on_resolution_options_item_selected(index):
 	selectedResolution = $ResolutionOptions.get_item_text(index)
@@ -71,3 +85,6 @@ func _on_back_button_pressed():
 # this grabs the focus when we switch to this scene
 func take_focus():
 	$ReferenceRect/ApplyButton.grab_focus()
+
+func _on_volume_slider_value_changed(value):
+	selectedVolume = $VolumeSlider.value
